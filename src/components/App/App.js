211a1e51f5e99e0c85e2c540018a0f1a97842b54
Justen Anderson <justenanderson@gmail.com>
2023-01-11 15:30:50 -0700

    Add apicalls and dummy data.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.scss'
 import NavBar from '../NavBar/NavBar'
 import Main from '../Main/Main'
 import MyMemes from '../MyMemes/MyMemes'
 import Footer from '../Footer/Footer'
-import getMemes from '../../apiCalls/apiCalls'
-import dummyData from '../../apiCalls/mock-data'
+// import getMemes from '../../apiCalls/apiCalls'
+// import dummyData from '../../apiCalls/mock-data'
 
 class App extends Component {
   constructor() {

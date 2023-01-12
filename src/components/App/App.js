46e5f1a41e4d7a3c1c77ec331575ac1027a28a65
Justen Anderson <justenanderson@gmail.com>
2023-01-12 10:47:38 -0700

    Fix fetch, update state in app.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,37 +5,47 @@ import Main from '../Main/Main'
 import MyMemes from '../MyMemes/MyMemes'
 import Footer from '../Footer/Footer'
 import getMemes from '../../apiCalls/apiCalls'
-import dummyData from '../../apiCalls/mock-data'
+// import dummyData from '../../apiCalls/mock-data'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-// const allMemes = dummyData.data.children
+
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = null
   }
 
   componentDidMount = () => {
     getMemes()
-  }
+      .then(data => {
+        console.log('Data: ',data)
+        this.setState({ memes: data.data.children})
+        console.log('state: ', this.state)
 
+      })
+      .catch(error => {
+        console.log('Error message from catch: ', error.message)
+        this.setState({ [error]: error.message })
+        console.log('state: ', this.state)
+      })
+  }
 
   render() {
     return (
       <Router>
-      <div className="app">
-        <NavBar />
-        <Switch>
-          <Route path="/my-memes">
-            <MyMemes />
-          </Route>
-          <Route path="/">
-            <Main allMemes= { this.state.dummyData } />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+        <div className="app">
+          <NavBar />
+          <Switch>
+            <Route path="/my-memes">
+              <MyMemes />
+            </Route>
+            <Route path="/">
+              <Main />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }

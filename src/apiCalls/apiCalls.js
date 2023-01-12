4c8bc674901d1e2949025f46b77100611301a129
Justen Anderson <justenanderson@gmail.com>
2023-01-12 10:19:45 -0700

    Convert fetch use async-await.

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,12 +1,26 @@
-
-
-const getMemes = () => {
-  return fetch('https://www.reddit.com/r/memes.json')
-    .then(response => {
-      if(!response.ok) {
-        return response.json()
-      }
-    })
+const getMemes = async () => {
+  const url = 'https://www.reddit.com/r/memes.json'
+  const response = await fetch(url)
+  if(!response.ok) {
+    throw new Error("Error retrieving data")
+  }
+  return response.json()
 }
+
+// const getMemes = () => {
+//   return fetch('https://www.reddit.com/r/memes.json')
+//     .then(response => {
+//       if(!response.ok) {
+//         throw new Error('Error in fetch')
+//       }
+//       return response.json()
+//     })
+//     .then(data => {
+//         console.log('Data: ', data)
+//       })
+//       .error(err => {
+//         console.log("Error: ", err.message)
+//       })
+// }
 
 export default getMemes
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,19 @@ import NavBar from '../NavBar/NavBar'
 import Main from '../Main/Main'
 import MyMemes from '../MyMemes/MyMemes'
 import Footer from '../Footer/Footer'
-// import getMemes from '../../apiCalls/apiCalls'
+import getMemes from '../../apiCalls/apiCalls'
 import dummyData from '../../apiCalls/mock-data'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-
+// const allMemes = dummyData.data.children
 
 class App extends Component {
   constructor() {
     super()
-    this.state = dummyData
+    this.state = {}
+  }
+
+  componentDidMount = () => {
+    getMemes()
   }
 
 

976f67ae658dca91698ada9c3d84ab8a3db55639
Justen Anderson <justenanderson@gmail.com>
2023-01-13 09:32:08 -0700

    Connect fetch to dummy data for development.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,30 @@ import Main from '../Main/Main'
 import MyMemes from '../MyMemes/MyMemes'
 import Footer from '../Footer/Footer'
 import getMemes from '../../apiCalls/apiCalls'
-// import dummyData from '../../apiCalls/mock-data'
+import dummyData from '../../apiCalls/mock-data'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
+const allMemes = dummyData.data.children
 
 class App extends Component {
   constructor() {
     super()
-    this.state = null
+    this.state = allMemes[0] //Using dummyData - Continue to test with fetch during development
+    // this.state = {} //Using dummyData - Continue to test with fetch during development
   }
 
-  componentDidMount = () => {
-    getMemes()
-      .then(data => {
-        console.log('Data: ',data)
-        this.setState({ memes: data.data.children})
-        console.log('state: ', this.state)
-
-      })
-      .catch(error => {
-        console.log('Error message from catch: ', error.message)
-        this.setState({ [error]: error.message })
-        console.log('state: ', this.state)
-      })
-  }
+    //  NOTE - Using dummyData - Continue to test with fetch during development// NOTE
+  // componentDidMount = () => {
+  //   getMemes()
+  //     .then(data => {
+  //       // this.setState({ memes: data.data.children})
+  //     })
+  //     .catch(error => {
+  //       console.log('Error message from catch: ', error.message)
+  //       this.setState({ [error]: error.message })
+  //       // console.log('state: ', this.state)
+  //     })
+  // }
 
   render() {
     return (
